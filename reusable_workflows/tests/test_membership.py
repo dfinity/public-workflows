1036db4eec82385228953607968ec85a9302be3f
Carly Gundy <carlene.gundy@dfinity.org>
2023-10-02 16:25:28 +0200

    update tests

--- reusable_workflows/tests/test_membership.py
+++ reusable_workflows/tests/test_membership.py
@@ -30,7 +30,7 @@ def test_not_member():
 
 
 @mock.patch.dict(
-    os.environ, {"GH_ORG": "my_org", "GH_TOKEN": "secret", "USER": "username"}
+    os.environ, {"GH_ORG": "my_org", "GITHUB_TOKEN": "secret", "USER": "username"}
 )
 @mock.patch("github3.login")
 @mock.patch("os.system")
@@ -52,7 +52,7 @@ def test_end_to_end_is_member(os_system, github_login_mock, capfd):
 
 
 @mock.patch.dict(
-    os.environ, {"GH_ORG": "my_org", "GH_TOKEN": "secret", "USER": "username"}
+    os.environ, {"GH_ORG": "my_org", "GITHUB_TOKEN": "secret", "USER": "username"}
 )
 @mock.patch("github3.login")
 @mock.patch("os.system")
@@ -74,7 +74,7 @@ def test_end_to_end_is_not_member(os_system, github_login_mock, capfd):
 
 
 @mock.patch.dict(
-    os.environ, {"GH_ORG": "my_org", "GH_TOKEN": "secret", "USER": "username"}
+    os.environ, {"GH_ORG": "my_org", "GITHUB_TOKEN": "secret", "USER": "username"}
 )
 @mock.patch("github3.login")
 @mock.patch("os.system")
@@ -90,7 +90,9 @@ def test_end_to_end_api_fails(os_system, github_login_mock):
         os_system.assert_not_called()
 
 
-@mock.patch.dict(os.environ, {"GH_ORG": "my_org", "GH_TOKEN": "", "USER": "username"})
+@mock.patch.dict(
+    os.environ, {"GH_ORG": "my_org", "GITHUB_TOKEN": "", "USER": "username"}
+)
 @mock.patch("github3.login")
 def test_github_token_not_passed_in(github_login_mock):
     github_login_mock.return_value = None
