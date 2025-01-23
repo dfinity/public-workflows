cab1af48e15c03b986fbb0321bf4de72a77a229d
Carly Gundy <carlene.gundy@dfinity.org>
2025-01-23 12:12:55 +0100

    try again

--- reusable_workflows/check_membership/check_external_contrib.py
+++ reusable_workflows/check_membership/check_external_contrib.py
@@ -16,7 +16,7 @@ def get_repos_open_to_contributions(gh: github3.login) -> List[str]:
     file_content = download_gh_file(repo, file_path)
 
     # convert .txt file to list and strip out comments
-    repo_list = [line.strip() for line in file_content.split("\n") if line.strip() and not line.strip().startswith("#")]
+    repo_list = [line.split("#")[0].strip() for line in file_content.split("\n") if line.split("#")[0].strip()]
     return repo_list
 
 
