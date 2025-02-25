4f0d80f102e95491e6c98b7a3ecf9459c9c73316
Carly Gundy <47304080+cgundy@users.noreply.github.com>
2025-02-25 11:05:57 +0100

    chore(IDX): update approved bots list (#111)
    * update approved bots list
    
    * Update test_membership.py

--- reusable_workflows/check_membership/check_membership.py
+++ reusable_workflows/check_membership/check_membership.py
@@ -9,8 +9,7 @@ APPROVED_BOT_LIST = [
     "github-actions[bot]",
     "gix-bot",
     "mergify[bot]",
-    "pr-automation-bot-public[bot]",
-    "pr-automation-bot-private[bot]",
+    "pr-creation-bot-dfinity[bot]",
     "sa-github-api",
 ]
 
