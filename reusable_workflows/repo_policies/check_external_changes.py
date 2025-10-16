4c0e9045dfce68fd2b73a2f625431e9d369dbce0
Bas van Dijk <bas@van.dijk.ch>
2025-10-16 11:27:18 +0200

    check

--- reusable_workflows/repo_policies/check_external_changes.py
+++ reusable_workflows/repo_policies/check_external_changes.py
@@ -3,14 +3,9 @@ import sys
 from pathlib import Path
 
 
-EXTERNAL_CONTRIB_BLACKLIST_PATH = "repo/.github/repo_policies/EXTERNAL_CONTRIB_BLACKLIST"
-
-CHANGED_FILES_PATH = ".github/outputs/added_files.txt"
-
-
 def main():
-    changed_files = Path(CHANGED_FILES_PATH).read_text().splitlines()
-    blacklist_files = Path(EXTERNAL_CONTRIB_BLACKLIST_PATH).read_text().splitlines()
+    changed_files = Path(".github/outputs/added_files.txt").read_text().splitlines()
+    blacklist_files = Path("repo/.github/repo_policies/EXTERNAL_CONTRIB_BLACKLIST").read_text().splitlines()
 
     if blacklist_files == []:
         print("No blacklisted files found.")
